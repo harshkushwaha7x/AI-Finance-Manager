bf5022bc2c729f1a7b5037c998901ec5bc76fa6f
Harsh Kushwaha <115409050+harshkushwaha7x@users.noreply.github.com>
2026-04-01 03:48:35 +0530

    create prisma singleton and shared database exports

---
+++ src/lib/db/index.ts
@@ -0,0 +1,3 @@
+export { prisma } from "@/lib/db/prisma";
+export { createSupabaseBrowserClient } from "@/lib/db/supabase-browser";
+export { createSupabaseAdminClient } from "@/lib/db/supabase-server";
